4821c5b87bbc146401d22e3eeb0f80083b6dfa65
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2020-08-13 12:49:13 -0700

    Add Winston

--- src/app.js
+++ src/app.js
@@ -31,6 +31,18 @@ app.use(express.json())
 app.use(helmet())
 app.use(cors())
 
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`)
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+}) 
+
 app.get('/', (req, res) => {
     res
         .send('A GET Request')
@@ -52,18 +64,6 @@ app.use(function errorHandler(err, req, res, next) {
     }
     res.status(500).json(response)
 })
-
-app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
-  
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        logger.error(`Unauthorized request to path: ${req.path}`)
-        return res.status(401).json({ error: 'Unauthorized request' })
-    }
-    // move to the next middleware
-    next()
-}) 
 
 const cards = [{
     id: 1,
